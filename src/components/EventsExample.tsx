54c6f611030c8e2ffa3b17843f5b3728649744ee
Zhannur <jannuraidynuly@gmail.com>
2022-01-30 15:51:04 +0500

    Hook typing useRef. Unmanaged Component

--- src/components/EventsExample.tsx
+++ src/components/EventsExample.tsx
@@ -1,15 +1,16 @@
-import React, { FC, useState } from "react";
+import React, { FC, useRef, useState } from "react";
 
 const EventsExample: FC = () => {
     const [value, setValue] = useState<string>('')
     const [isDrag, setIsDrag] = useState<boolean>(false);
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         setValue(e.target.value)
     }
 
     const clickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        console.log(value)
+        console.log(inputRef.current?.value)
     }
 
     const dragHandler = (e: React.DragEvent<HTMLDivElement>) => {
@@ -34,7 +35,8 @@ const EventsExample: FC = () => {
 
     return (
         <div>
-            <input type="text" value={value} onChange={changeHandler}/>
+            <input type="text" value={value} onChange={changeHandler} placeholder="Управляемый"/>
+            <input type="text" ref={inputRef} placeholder="Неуправляемый"/>
             <button onClick={clickHandler}>Dina</button>
             <div onDrag={dragHandler} draggable style={{width: 200, height: 200, background: 'red'}}></div>
             <div 
