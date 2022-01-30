d7cab6b7e13ccd795b30270572b908421d65f617
Zhannur <jannuraidynuly@gmail.com>
2022-01-30 11:37:50 +0500

    React function component type

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import Card from "./components/Card";
+import Card, { CardVariant } from "./components/Card";
 
 const App = () => {
   return (
     <div>
-      <Card width="200px" height="200px">
+      <Card
+        onClick={(num) => console.log('click', num)}
+        variant={CardVariant.outlined} 
+        width="200px" 
+        height="200px"
+      >
         <button>Click</button>
       </Card>
     </div>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,33 @@
+import { FC, useState } from "react";
+
+export enum CardVariant {
+    outlined = 'outlined',
+    primary = 'primary'
+}
+
 interface CardProps {
     width?: string;
     height?: string;
-    children?: React.ReactChild | React.ReactNode
+    variant: CardVariant,
+    onClick: (num: number) => void
 }
 
-const Card = ({width, height, children}: CardProps) => {
+const Card: FC<CardProps> = 
+    ({
+        width, 
+        height,
+        variant,
+        onClick,
+        children
+    }) => {
+    const [state, setState] = useState(0)
+    
     return (
-        <div style={{width, height, border: '1px solid gray'}}>
+        <div style={{width, height, 
+            border: variant === CardVariant.outlined ? '1px solid gray' : 'none',
+            background: variant === CardVariant.primary ? 'lightgray' : ''}}
+            onClick={() => onClick(state)}
+        >
             {children}
         </div>
     );
